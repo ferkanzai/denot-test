517c916eb481729e7186b4c4d53fc907ec15d2c4
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2022-02-17 02:19:01 +0100

    feat: remove dotenv and dinoenv as not supported by deno deploy

--- src/deps.ts
+++
@@ -1,3 +0,0 @@
-import { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-
-export { dotEnvConfig };
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import * as env from "https://deno.land/x/dinoenv@v1.1.0/mod.ts";
 import router from "./routes.ts";
 
-env.config({ path: './.env'});
 const app = new Application();
 
 app.use(router.routes());
