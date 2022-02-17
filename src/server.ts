570c7ba8ef55a0ab4b9ffcabc57af40aad11ebae
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2022-02-17 02:23:14 +0100

    feat: add dotenv back only in local

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { dotEnvConfig } from "./deps.ts";
 import router from "./routes.ts";
+
+const production = Deno.env.get("PRODUCTION");
+
+if (production !== "true") {
+  dotEnvConfig({ export: true });
+}
 
 const app = new Application();
 
