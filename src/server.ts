c0b91d788b3b493b4db44c1e6e62ce504570e5de
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2022-02-17 02:15:29 +0100

    feat: change dotenv for dinoenv

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { dotEnvConfig } from "./deps.ts";
+import * as env from "https://deno.land/x/dinoenv@v1.1.0/mod.ts";
 import router from "./routes.ts";
 
-dotEnvConfig({ export: true });
+env.config({ path: './.env'});
 const app = new Application();
 
 app.use(router.routes());
